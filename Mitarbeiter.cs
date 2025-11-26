1cd232f521e612be51b9c49780f15cac9e887139
Remziberisha <Remzi.berisha@training.lug-ag.de>
2025-11-26 17:54:03 +0100

    Die ersten änderungen

---
+++ Mitarbeiter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalVerwaltungssystem
+{
+    internal class Mitarbeiter
+    {
+        string name {  get; set; }
+        string nachNAme { get; set; }
+
+        int urlaubstage { get; set; }
+        int krankheitstage { get; set; }
+
+        public Mitarbeiter(string name, string nachNAme, int urlaubstage, int krankheitstage)
+        {
+            this.name = name;
+            this.nachNAme = nachNAme;
+            this.urlaubstage = urlaubstage;
+            this.krankheitstage = krankheitstage;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace PersonalVerwaltungssystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+           
         }
     }
 }
